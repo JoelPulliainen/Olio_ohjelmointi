35991a996c846faaf6736fa409107c67f6bdb4d7
JoelPulliainen <66187673+JoelPulliainen@users.noreply.github.com>
2020-05-31 04:42:18 +0300

    Add files via upload
    V2

--- Mainclass.java
+++ Mainclass.java
@@ -1,6 +1,17 @@
 
+
+
+
 public class Mainclass {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		
+		String speech1 = "Hau";
+		String speech2 = "Vuh";
+		
+		Dog dog1 = new Dog("Rekku");
+		Dog dog2 = new Dog("Musti");
+		
+		dog1.speak(speech1);
+		dog2.speak(speech2);
 	}
 }
