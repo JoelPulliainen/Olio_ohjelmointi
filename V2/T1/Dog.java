9563c05482d5cc538ce1ff07a2f36828e85842c0
JoelPulliainen <66187673+JoelPulliainen@users.noreply.github.com>
2020-05-31 04:47:58 +0300

    Add files via upload

---
+++ V2/T1/Dog.java
@@ -0,0 +1,13 @@
+
+public class Dog {
+
+	
+	public Dog (String n) {
+		System.out.println("Hey, my name is " + n + "!");
+	}
+	
+	public void speak(String s) {
+		System.out.println(s + "!");
+	}
+	
+}
