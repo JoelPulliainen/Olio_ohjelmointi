b4b56cd7a3b2eeb65dfe5120eafa03dd2857cf4b
JoelPulliainen <66187673+JoelPulliainen@users.noreply.github.com>
2021-02-06 13:50:22 +0200

    Update ReadAndWriteIO.java

--- V4/T1/ReadAndWriteIO.java
+++ V4/T1/ReadAndWriteIO.java
@@ -1,5 +1,26 @@
 package Tehtavat;
 
-public class ReadAndWriteIO {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class ReadAndWriteIO {
+    public void readFile(String FileName) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(FileName));
+			String outputbuffer = "";
+			while ((outputbuffer = br.readLine()) != null) {
+				System.out.println(outputbuffer);
+			}
+			br.close();
+			
+		} catch (FileNotFoundException ex) {
+			Logger.getLogger(ReadAndWriteIO.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (IOException ex) {
+			Logger.getLogger(ReadAndWriteIO.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
