c638ae3339f14d21372e04e940c84d62f9ed19f8
JoelPulliainen <66187673+JoelPulliainen@users.noreply.github.com>
2021-02-14 18:55:35 +0200

    Add files via upload

---
+++ V6/T4/Account.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mainclass;
+
+/**
+ *
+ * @author joelp
+ */
+abstract class Account {
+    public abstract String getName();
+    public abstract int getMoney();
+    public abstract void depositMoney(int m);
+    public abstract void withdrawMoney(int m);
+}
+
+
+class RegularAccount extends Account {
+    String name;
+    int amoney;
+    public RegularAccount(String accountname, int money) {
+        name = accountname;
+        amoney = money;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public int getMoney() {
+        return amoney;
+    }
+    public void depositMoney(int m) {
+        amoney += m;
+    }
+    public void withdrawMoney(int m) {
+        amoney -= m;
+    }
+}
+
+class CreditAccount extends Account {
+    String name;
+    int amoney;
+    int creditlimit;
+    public CreditAccount(String accountname, int money, int credit) {
+        name = accountname;
+        amoney = money;
+        creditlimit = credit;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public int getMoney() {
+        return amoney;
+    }
+    public void depositMoney(int m) {
+        amoney += m;
+    }
+    public void withdrawMoney(int m) {
+        amoney -= m;
+    }
+}
